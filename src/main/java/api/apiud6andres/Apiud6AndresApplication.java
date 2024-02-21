cc9ee828c67c9d82bf03a23ba7b752ca8ef3ad77
andres <andres.rivada@iessanmamede.com>
2024-02-21 15:19:30 +0100

    Initial commit

---
+++ src/main/java/api/apiud6andres/Apiud6AndresApplication.java
@@ -0,0 +1,13 @@
+package api.apiud6andres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiud6AndresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apiud6AndresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/apiud6andres/Apiud6AndresApplicationTests.java
@@ -0,0 +1,13 @@
+package api.apiud6andres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apiud6AndresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
